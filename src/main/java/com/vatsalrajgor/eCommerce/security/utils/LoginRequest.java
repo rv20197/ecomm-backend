2abebc02ec4fed4c5dcf4a423a8982bb34b2c40d
Vatsal <rv20197@gmail.com>
2026-02-05 08:10:10 +0530

    Refactored LoginRequest and LoginResponse classes to use Lombok annotations for getters and setters

--- src/main/java/com/vatsalrajgor/eCommerce/security/utils/LoginRequest.java
+++ src/main/java/com/vatsalrajgor/eCommerce/security/utils/LoginRequest.java
@@ -1,23 +1,12 @@
 package com.vatsalrajgor.eCommerce.security.utils;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class LoginRequest {
     private String username;
     private String password;
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
 }
--- src/main/java/com/vatsalrajgor/eCommerce/security/utils/LoginResponse.java
+++ src/main/java/com/vatsalrajgor/eCommerce/security/utils/LoginResponse.java
@@ -1,35 +1,16 @@
 package com.vatsalrajgor.eCommerce.security.utils;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
+@Getter
+@Setter
 public class LoginResponse {
     private String token;
     private String userName;
     private List<String> roles;
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public List<String> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(List<String> roles) {
-        this.roles = roles;
-    }
 
     public LoginResponse(String token, String userName, List<String> roles) {
         this.token = token;
