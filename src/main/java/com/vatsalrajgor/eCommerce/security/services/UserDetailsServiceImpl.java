b6650244cfbbe7adca9b60f95d2e1cf66c2efd28
Vatsal <rv20197@gmail.com>
2026-02-05 08:44:49 +0530

    Added `UserDetailsServiceImpl` and `UserRepository` for Spring Security integration
    - Implemented `UserDetailsService` interface to load user details by username.
    - Created `UserRepository` with a method to find users by username.

---
+++ src/main/java/com/vatsalrajgor/eCommerce/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.vatsalrajgor.eCommerce.security.services;
+
+import com.vatsalrajgor.eCommerce.model.User;
+import com.vatsalrajgor.eCommerce.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+}
